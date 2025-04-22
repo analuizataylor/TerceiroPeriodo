e53ce3a4684521f6a877e6321481683292a81598
Ana Luiza Menelli Taylor <analuizamenelli@gmail.com>
2025-04-21 22:45:08 -0300

    Add files via upload

---
+++ src/listasimplesmenteencadeada_item/Item.java
@@ -0,0 +1,41 @@
+package listasimplesmenteencadeada_item;
+
+public class Item {
+	// atributos
+	private int codigo;
+	private String nome;
+	
+	// construtor
+	// NomeDaClasse + (tipo + atributo)
+	public Item(int codigo, String nome) {
+		super();
+		// this.atributo = atributo;
+		this.codigo = codigo;
+		this.nome = nome;
+	}
+	
+	// getAtributo + return = função
+	public int getCodigo() {
+		return codigo;
+	}
+	
+	// setAtributo + tipo atributo = procedimento
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	// String + toString + return atributos
+	@Override
+	public String toString() {
+		return "Codigo=" + codigo + ", Nome=" + nome;
+	}
+	
+}
